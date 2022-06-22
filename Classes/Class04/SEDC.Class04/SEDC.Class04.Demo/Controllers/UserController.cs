433e977d74dd2a0509e8009f1b0edb771f046a05
anetastankovska <anetastankovskaane@gmail.com>
2022-06-22 20:51:51 +0200

    Class code

---
+++ Classes/Class04/SEDC.Class04/SEDC.Class04.Demo/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.Class04.Demo.Models;
+
+namespace SEDC.Class04.Demo.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            User user = new User()
+            {
+                Id = 1,
+                FirstName = "Aneta",
+                LastName = "Stankovska",
+                Age = 30,
+                City = "Skopje",
+                IsEmployed = true
+            };
+            return View(user);
+        }
+    }
+}
