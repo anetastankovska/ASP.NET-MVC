e8f53a0361cd629c7d49e4053db45ec24a885498
anetastankovska <anetastankovskaane@gmail.com>
2022-06-29 21:02:02 +0200

    PizzaAppRefactor

---
+++ SEDC.PizzaApp.Refactored/SEDC.PizzaApp/SEDC.PizzaApp.Domain/Models/PizzaOrder.cs
@@ -0,0 +1,17 @@
+﻿using SEDC.PizzaApp.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Domain.Models
+{
+    public class PizzaOrder : BaseEntity
+    {
+        public int PizzaId { get; set; }
+        public Pizza Pizza { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+}
---
+++ SEDC.PizzaApp.Refactored/SEDC.PizzaApp/SEDC.PizzaApp.Services/Services/OrderService.cs
@@ -0,0 +1,32 @@
+﻿using SEDC.PizzaApp.DataAccess.Data;
+using SEDC.PizzaApp.DataAccess.Repositories.Interfaces;
+using SEDC.PizzaApp.Domain.Models;
+using SEDC.PizzaApp.Services.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Services.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IRepository<Order> _orderRepository;
+
+        public OrderService(IRepository<Order> orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public List<Order> GetAllOrders()
+        {
+            return _orderRepository.GetAll();
+        }
+
+        public Order GetOrderById(int id)
+        {
+            return _orderRepository.GetById(id);
+        }
+    }
+}
