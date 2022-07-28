0e4a109c870a1d379df1bff0b8594a745f0674fa
anetastankovska <anetastankovskaane@gmail.com>
2022-07-28 15:47:31 +0200

    Models created

---
+++ BurgerAppRefactored/BurgerApp.Refactored/Domain/Models/Burger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Burger : BaseEntity
+    {
+        public string Name { get; set; }
+        public bool IsOnPromotion { get; set; }
+        public double Price { get; set; }
+        public List<BurgerOrder> BurgerOrders { get; set; }
+    }
+}
---
+++ BurgerAppRefactored/BurgerApp.Refactored/Domain/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Order : BaseEntity
+    {
+        public string Address { get; set; }
+        public bool IsDelivered { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; } 
+        public PaymentMethod PaymentMethod { get; set; }
+        public List<BurgerOrder> BurgerOrders { get; set; }
+    }
+}
